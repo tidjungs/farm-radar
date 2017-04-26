470265af02afcdcf64aafade2de866fa63ee2627
tidjungs <waewprach.s@ku.th>
2017-04-27 00:26:24 +0700

    config eslint airbnb

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -5,5 +6,5 @@ import './index.css';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
