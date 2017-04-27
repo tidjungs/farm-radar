bcbd5cbf45bae970b0402157a1baa65cefbd187a
tidjungs <waewprach.s@ku.th>
2017-04-27 21:16:58 +0700

    add more data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LineChart from './LineChart';
 import mapColorWithFarm from './utils/color';
 import MovingAvgCard from './MovingAvgCard';
+
+const farm = [
+  { id: 1, name: 'farm1', mAvg: '$4000', color: '#9b59b6', active: true },
+  { id: 2, name: 'farm2', mAvg: '$3500', color: '#e74c3c', active: true },
+  { id: 3, name: 'farm3', mAvg: '$3500', color: '#2ecc71', active: true },
+  { id: 4, name: 'farm4', mAvg: '$6000', color: '#f1c40f', active: true },
+];
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       data: [
-        { id: 1, name: 'JAN', uv: 4000, pv: 2400 },
-        { id: 2, name: 'FEB', uv: 3000, pv: 1398 },
-        { id: 3, name: 'MAR', uv: 2000, pv: 9800 },
-        { id: 4, name: 'APR', uv: 2780, pv: 3908 },
-        { id: 5, name: 'MAY', uv: 1890, pv: 4800 },
-        { id: 6, name: 'JUN', uv: 2390, pv: 3800 },
-        { id: 7, name: 'JUL', uv: 3490, pv: 4300 },
+        { id: 1, name: 'JAN', farm1: 4000, farm2: 2400, farm3: 1600, farm4: 3200 },
+        { id: 2, name: 'FEB', farm1: 3000, farm2: 1398, farm3: 1800, farm4: 2500 },
+        { id: 3, name: 'MAR', farm1: 2000, farm2: 9800, farm3: 2400, farm4: 1600 },
+        { id: 4, name: 'APR', farm1: 2780, farm2: 3908, farm3: 3600, farm4: 2500 },
+        { id: 5, name: 'MAY', farm1: 1890, farm2: 4800, farm3: 1700, farm4: 3300 },
+        { id: 6, name: 'JUN', farm1: 2390, farm2: 3800, farm3: 2200, farm4: 3000 },
+        { id: 7, name: 'JUL', farm1: 3490, farm2: 4300, farm3: 1300, farm4: 3200 },
       ],
-      farm: [
-        { id: 1, name: 'uv', mAvg: 4000, color: '#9b59b6', active: true },
-        { id: 2, name: 'pv', mAvg: 3500, color: '#e74c3c', active: true },
-      ],
+      farm: mapColorWithFarm(farm),
     };
   }
 
@@ -37,7 +40,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          FARM RADAR
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -46,6 +49,8 @@ class App extends Component {
           <div className="line-graph-container">
             <LineChart data={this.state.data} farm={mapColorWithFarm(this.state.farm)} />
           </div>
+          <h1>MOVING AVERAGE</h1>
+          <br />
           <div className="moving-avg-container">
             {
               this.state.farm.map(f =>
@@ -62,6 +67,9 @@ class App extends Component {
             }
           </div>
         </div>
+        <br />
+        <br />
+        <br />
       </div>
     );
   }
