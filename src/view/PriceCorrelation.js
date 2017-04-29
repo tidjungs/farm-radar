8d5eaaad437f30243f3e184e58b068db3238b863
tidjungs <waewprach.s@ku.th>
2017-04-29 14:14:23 +0700

    add PriceCorelation boiler

--- src/view/PriceCorrelation.js
+++ src/view/PriceCorrelation.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TimeSelect from '../component/TimeSelect';
 
-const PriceCorrelation = () => (
-  <div>
-    Correlation
-  </div>
-);
+class PriceCorrelation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      duration: [
+        { id: 1, name: '1W', active: true },
+        { id: 2, name: '1M', active: false },
+        { id: 3, name: '1Y', active: false },
+        { id: 4, name: '2Y', active: false },
+        { id: 5, name: '5Y', active: false },
+      ],
+    };
+  }
+
+  changeDuration(index) {
+    this.setState({
+      duration: this.state.duration.map(d =>
+        (d.id === index ? { ...d, active: true } : { ...d, active: false }),
+      ),
+    });
+  }
+
+  render() {
+    return (
+      <div className="content-container">
+        <TimeSelect
+          duration={this.state.duration}
+          changeDuration={index => this.changeDuration(index)}
+        />
+        Correlation
+      </div>
+    );
+  }
+}
 
 export default PriceCorrelation;
