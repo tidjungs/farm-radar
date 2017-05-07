fd78cc44557dea55a074b20f80e8fb1121a4c0f9
tidjungs <waewprach.s@ku.th>
2017-05-07 22:04:58 +0700

    config sidebar style

--- src/component/SearchCard.js
+++ src/component/SearchCard.js
@@ -3,9 +3,12 @@ import React from 'react';
 const SearchCard = ({ productName, selectProduct, active }) => (
   <div
     className="search-card"
-    style={active ? { background: '#A0E9D4' } : { background: 'none' }}
+    style={active ? { background: '#e0e0e0' } : {}}
   >
-    <button className="product-btn" onClick={() => selectProduct()}>
+    <button
+      className="product-btn" onClick={() => selectProduct()}
+      style={active ? { color: '#17B897' } : { color: '#e0e0e0' }}
+    >
       { productName }
     </button>
   </div>
